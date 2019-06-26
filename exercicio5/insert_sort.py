9a1a75be9a890893587301057f6a32ed21a14cef
Ranyelson Dantas <nielsonnp@gmail.com>
2019-06-26 09:58:14 -0300

    Exemplo: insert sort

--- exercicio5/insert_sort.py
+++ exercicio5/insert_sort.py
@@ -1,17 +1,19 @@
 
 #Insert Sort
 
-lista = [30, 50, 10, 35, 70, 45, 80, 100, 22]
 
-def insertionSort(lista):
+lista = [54,26,93,17,77,31,44,55,20]
+
+def insertion_sort(lista):
     for i in range(1,len(lista)):
         x = lista[i]
         j = i-1
-        while j>=0 and x<lista[j]:
-            lista[j+1] = lista[j]
-            j=j-1
-        lista[j+1] = x
+        while j > 0 and lista[j-1] > x:
+            lista[j] = lista[j-1]
+            j = j-1
+            lista[j] = x
 
+        print("Passo", i, lista)
+        input()
 
-insertionSort(lista)
-print(lista)
+insertion_sort(lista)
